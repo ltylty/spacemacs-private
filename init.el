875b6400803784dfbe937efcc366f24f4631cbdb
tianyuanLiu <tianyuanLiu@hotmail.com>
2016-08-19 09:29:21 +0800

    proxy

--- init.el
+++ init.el
@@ -241,10 +241,8 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
-  (setq configuration-layer--elpa-archives
-        '(("melpa-cn" . "http://elpa.zilongshanren.com/melpa/")
-          ("org-cn"   . "http://elpa.zilongshanren.com/org/")
-          ("gnu-cn"   . "http://elpa.zilongshanren.com/gnu/")))
+  (setq url-proxy-services '(("https" . "127.0.0.1:8087")
+                             ("http" . "127.0.0.1:8087")))
   )
 
 (defun dotspacemacs/user-config ()
